1281bc5449b56b84ed2c86aa6bf12da71343745b
ilmi-p <iveliu96@gmail.com>
2024-12-09 10:55:47 +0000

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * binary_tree_node - create a binary tree node
  * @parent: the parent node
@@ -7,10 +8,12 @@
  *
  * Return: pointer to binary newlyt created tree node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->parent = parent;
 	new_node->n = value;
