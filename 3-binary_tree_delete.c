29da584a8a3ddf601b5d47cbe6acacfa9c9b6db3
korneel <korneel.loy@gmail.com>
2024-12-09 15:27:52 +0100

    ex 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deleting entire binary_tree
+ * @tree: pointer to the tree to be deleted
+ *
+ * Return: no return
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
