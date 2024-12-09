e6b6eb15a89ecaf5305533a85541085668e8a62e
korneel <korneel.loy@gmail.com>
2024-12-09 10:29:40 +0100

    start

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: the parent node
+ * @value: the value of the node
+ *
+ * Return: pointer to binary newlyt created tree node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
