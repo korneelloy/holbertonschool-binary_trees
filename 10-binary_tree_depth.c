d7b403181e5126c6e8b1b47d8b0ff694374ae2f7
ilmi-p <iveliu96@gmail.com>
2024-12-10 09:54:27 +0000

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,14 +9,15 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t  tree = 0;
+	size_t  count = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	while (tree->parent != NULL)
 	{
-		tree ++;
+		count ++;
+		tree = tree->parent;
 	}
-	return (tree);
+	return (count);
 }
