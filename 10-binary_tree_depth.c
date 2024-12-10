a1553c38403d34681d9c4dccdb99449975384520
ilmi-p <iveliu96@gmail.com>
2024-12-10 09:46:09 +0000

    name of count rename

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,17 +9,14 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t  count = 0;
+	size_t  tree = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	while (tree->parent != NULL)
 	{
-		count ++;
-
-		
+		tree ++;
 	}
-
-	return (count);
+	return (tree);
 }
